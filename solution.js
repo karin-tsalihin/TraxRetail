5fde67e1595bdadf860ff71e00d08fd50faa99e2
karin-tsalihin <>
2020-10-25 14:30:00 +0200

    feat: route added for adding new contact
    body-parser module was installed

--- solution.js
+++ solution.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const server = express();
 
+//require body parser module for PSOT request and create parser
+const bodyParser = require('body-parser');
+// extended option is false- user's input is expected to be a contact object 
+// client side is required (for example: html form)
+let urlencodedParser = bodyParser.urlencoded({extended:false});
+
 // defining port number
 const port = 4000;
 
@@ -19,14 +25,24 @@ let contacts = [
 {name: 'Dana', tel: '0556625034'},
 ];
 
+
 // respond with a message on http://localhost:4000/whos-there
 server.get('/whos-there',(req,res) => {
 	res.send('Hi Trax! This is Karin Tsalihin');
 });
 
-// create a new route for getting all the contacts
+// a route for getting all the contacts
 server.get('/AllContacts',(req,res) => {
 	res.send(contacts);
+});
+
+// a route for adding a new contact (from client side) to contacts array
+server.post('/AddContact', urlencodedParser, function(req, res){
+	new_contact = req.body; 
+	// check for errors in the input (invalid or contact that is already exists in contacts)
+	contacts.push(new_contact);
+	// send respone to the user- status: success
+	res.status(200);
 });
 
 
